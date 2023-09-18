c3af08d169f59fb9fc7cdce3f4c00acd5228da4c
Jezequel_Maxence <maxence.jezequel@hotmail.com>
2023-09-18 16:01:41 +0200

    Page 404 et amélioration du header et footer

--- src/components/404/404.jsx
+++ src/components/404/404.jsx
@@ -3,10 +3,10 @@ import '../../styles/404.css'
 
 export default function ErrorPage() {
 	return (
-		<div className="errorContainer">
-			<p className="errorNumber">404</p>
-			<p className="errorText">Oups! La page que vous demandez n'existe pas.</p>
-			<Link to="/" className="errorLinkHome">
+		<div className="error-container">
+			<h1 className="error-number_text">404</h1>
+			<p className="error-description_text">Oups! La page que vous demandez n'existe pas.</p>
+			<Link to="/" className="error-linkhome">
 				Retourner sur la page d’accueil
 			</Link>
 		</div>
