1f586b273d1168c02c79679c5e3277c3c4fc5f8d
Jezequel_Maxence <maxence.jezequel@hotmail.com>
2023-09-18 15:31:13 +0200

    Ajout du Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,6 +5,7 @@ import logo from '../../assets/Logo.svg'
 
 export default function Header() {
 	return (
+		/*header */
 		<header className="header-container">
 			<img className= "header-logo" src={logo} alt="Kasa Logo" />
 			<Nav className="nav-header" />
